95dae42d1f9d17f751cee3440de3ab4a4e68cb82
alfredochiu <petercy32@gmail.com>
2023-11-23 13:37:17 +0800

    datareorgan

---
+++ initial copy.py
@@ -0,0 +1,23 @@
+import time 
+start_time = time.perf_counter()
+# import dell_dock
+# import dell_NB
+# import dell_DT
+
+# import lenovo
+# import hp
+
+import json_Hp_Dock
+import json_Hp_NB
+import json_Hp_DT
+
+import json_Lenovo_Dock
+import json_Lenovo_NB
+import json_Lenovo_DT
+
+import reorganize_data
+
+
+end_time = time.perf_counter()
+elapsed_time = end_time - start_time
+print(f"took {elapsed_time:.2f} seconds to run.")
